caed82dcb02aeb77be6d170bf6c557c5db714112
Amy M. Giglio <agiglio@PARISH.LOCAL>
2020-04-04 23:41:44 -0400

    worked on collision detection and mobile array

--- apps.js
+++ apps.js
@@ -1,62 +1,182 @@
-///////////////////// CONSTANTS /////////////////////////////////////
+///////////////////// LEVELS
+
+let levels = [
+//level one
+  {
+    blocks: [
+      {
+        x: 0,
+        y: 470,
+        width: 800,
+        height: 30,
+        color: "green",
+      },
+      {
+        x: 350,
+        y: 400,
+        width: 50,
+        height: 70,
+        color: "brown",
+      },
+    ],
+    enemies: [
+
+    ]
+  },
+//level two
+]
+
+///////////////////// OBJECTS
 
 let user = {
   x: 30,
-  y: 350,
+  y: 410,
+  walkLeft: false,
+  walkRight: false,
+  touchLeft: false,
+  touchRight: false,
+  touchUp: false,
+  touchDown: false,
+  isJumping: false,
+  speed: 3,
   canFall: false,
   jump: function() {
     user.canFall = true;
   },
-  standing: function() {
-    ctx.beginPath();
-    ctx.rect(user.x, user.y, 50, 120);
-    ctx.fill()
-    ctx.closePath();
+  standing: userStanding,
   },
 
 }
 
-///////////////////// APP STATE (VARIABLES) /////////////////////////
+///////////////////// VARIABLES
 
+let currentLevel = 0;
+let mobile = [user];
+let onScreenThings = [];
 
-
-///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+///////////////////// ELEMENTS
 
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
 
-///////////////////// EVENT LISTENERS ///////////////////////////////
+///////////////////// EVENT LISTENERS
 
 canvas.onclick = runTicks;
-document.addEventListener("keydown", keys);
+// document.getElementsByClassName("inactive").onclick = runTicks;
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
 
-///////////////////// ENGINE /////////////////////////////////////
+///////////////////// ENGINE
 
 function runTicks() {
+  console.log("ghjf");
+  document.getElementsByClassName("inactive").className = "active";
   setInterval(perTick, 1)
 };
 
 function perTick() {
-  gravity()
+  getMobile();
+  getOnScreenThings();
+  mobile.forEach((d) => {
+    collision(d);
+  });
+
+  move();
+  gravity();
   //  these are rendering functions
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  user.standing()
+  user.standing();
+  drawBlocks();
+};
+
+function drawBlocks() {
+  levels[currentLevel].blocks.forEach((block) => {
+    if (block.x <= canvas.width) {
+      ctx.beginPath();
+      ctx.rect(block.x, block.y, block.width, block.height);
+      ctx.fillStyle = block.color;
+      ctx.fill()
+      ctx.closePath();
+    }
+  });
+}
+
+function collision(e) {
+  onScreenThings.forEach((thing) => {
+    // collisions from the left
+    if (e.x + e.width === thing.x && e.y >= thing.y && e.y <= thing.y + thing.height && e.y + e.height >= thing.y && e.y + e.height <= thing.y + thing.height) {
+      if (levels[currentLevel].blocks.includes(thing)) {
+        e.walkLeft = false;
+      }
+      if (levels[currentLevel].enemies.includes(thing)) {
+        userDeath()
+      }
+    }
+
+    // collisions from the right
+
+  });
 }
 
 function gravity() {
 
+};
+
+function userDeath() {
+
 }
 
-function keys(a) {
+function move() {
+  mobile.forEach((c) => {
+    if (c.walkLeft) {
+      c.x -= c.speed;
+    };
+
+    if (c.walkRight) {
+      c.x += c.speed;
+    };
+  });
+};
+
+function getMobile() {
+  mobile = [user];
+  (levels[currentLevel].enemies.forEach((enemy) => {
+    if(enemy.x + enemy.width > 0 && enemy.x < 801)
+  });
+}
+
+function keyDown(a) {
   if (a.key === "ArrowRight"){
-    user.x++;
+    user.walkLeft = false;
+    user.walkRight = true;
   };
+
   if (a.key === "ArrowLeft"){
-    user.x--;
+    user.walkRight = false;
+    user.walkLeft = true;
   };
+
   if (a.key === "ArrowUp"){
-    user.jump();
+    user.isJumping = true;
   };
 };
 
-///////////////////// LEVELS /////////////////////////////////////
+function keyUp(b) {
+  if (b.key === "ArrowRight"){
+    user.walkRight = false;
+  };
+
+  if (b.key === "ArrowLeft"){
+    user.walkLeft = false;
+  };
+}
+
+///////////////////// SPRITES
+
+function userStanding() {
+  ctx.beginPath();
+  ctx.rect(user.x, user.y, 25, 60);
+  ctx.fillStyle = "black";
+  ctx.fill()
+  ctx.closePath();
+},
