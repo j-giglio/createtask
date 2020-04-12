03c1387bf6e6ce752895e6c3b604197ca199ab0d
Amy M. Giglio <agiglio@PARISH.LOCAL>
2020-04-12 18:00:37 -0400

    made entities block a thing

--- apps.js
+++ apps.js
@@ -5,30 +5,34 @@ let levels = [
   {
     startX: 30,
     startY: 410,
-    blocks: [
-      {
-        x: 0,
-        y: 470,
-        width: 800,
-        height: 30,
-        color: "green",
-      },
-      {
-        x: 350,
-        y: 400,
-        width: 50,
-        height: 70,
-        color: "brown",
-      },
-    ],
-    enemies: [
-      {
-        x: 450,
-        y: 300,
-        type: 1/*enemy1*/,
+    entities: {
+      blocks: [
+        {
+          x: 0,
+          y: 470,
+          width: 800,
+          height: 30,
+          color: "green",
+        },
+        {
+          x: 350,
+          y: 400,
+          width: 50,
+          height: 70,
+          color: "brown",
+        },
+      ],
+      enemies: [
+        {
+          x: 450,
+          y: 300,
+          type: 1/*enemy1*/,
 
-      },
-    ],
+        },
+      ],
+      // pickups: [];
+
+    }
   },
 //level two
 ]
@@ -42,6 +46,8 @@ let user = {
   height: 60,
   walkLeft: false,
   walkRight: false,
+  canWalkLeft: true,
+  canWalkRight: true,
   // touchLeft: false,
   // touchRight: false,
   // touchUp: false,
@@ -56,9 +62,14 @@ let user = {
   sprite: null,
 };
 
-// let enemy1 = {
-//   sprite:
-// };
+let enemy1 = {
+  sprite: 1,
+};
+
+///////////////////// ELEMENTS
+
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
 
 ///////////////////// VARIABLES
 
@@ -66,13 +77,10 @@ let currentLevel = 0;
 let mobile = [user];
 let onScreenThings = [];
 let tickCount = 0;
+// let leftScrollMargin = 30;
+// let rightScrollMargin = canvas.width /2;
+//////Sprite Animation Counters
 let sprUserWalkingFrame = 1;
-
-
-///////////////////// ELEMENTS
-
-const canvas = document.getElementById("board");
-const ctx = canvas.getContext("2d");
 
 ///////////////////// EVENT LISTENERS
 
@@ -109,7 +117,7 @@ function perTick() {
 };
 
 function drawBlocks() {
-  levels[currentLevel].blocks.forEach((block) => {
+  levels[currentLevel].entities.blocks.forEach((block) => {
     if (block.x <= canvas.width) {
       ctx.beginPath();
       ctx.rect(block.x, block.y, block.width, block.height);
@@ -122,14 +130,18 @@ function drawBlocks() {
 
 function collision(e) {
   onScreenThings.forEach((thing) => {
-    if (e.x < thing.x + thing.width &&
-        e.x + e.width > thing.x &&
-        e.y < e.y + thing.height && ///https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection November 4 2019
-        e.y + e.height > thing.y) {
-      if (levels[currentLevel].blocks.includes(thing)) {
-        e.walkRight = false;
+    if (e.x < thing.x + thing.width &&           ///
+        e.x + e.width > thing.x &&               ///
+        e.y < e.y + thing.height &&              ///https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection November 4 2019
+        e.y + e.height > thing.y) {              ///
+      if (levels[currentLevel].entities.blocks.includes(thing)) {
+        e.canWalkRight = false;
+        e.canWalkLeft = false;
+      } else {
+        e.canWalkRight = true;
+        e.canWalkLeft = true;
       }
-      if (levels[currentLevel].enemies.includes(thing)) {
+      if (levels[currentLevel].entities.enemies.includes(thing)) {
         userDeath()
       }
     }
@@ -146,12 +158,12 @@ function userDeath() {
 
 function move() {
   mobile.forEach((c) => {
-    if (c.walkLeft) {
-      if (c === user && c.x === 30) {
-        levels[currentLevel].blocks.forEach((block) => {
+    if (c.walkLeft && c.canWalkLeft) {
+      if (c === user && c.x === 30/*leftScrollMargin*/) {
+        levels[currentLevel].entities.blocks.forEach((block) => {
           block.x += user.speed;
         });
-        levels[currentLevel].enemies.forEach((enemy) => {
+        levels[currentLevel].entities.enemies.forEach((enemy) => {
           enemy.x += user.speed;
         });
       } else {
@@ -159,12 +171,12 @@ function move() {
       }
     };
 
-    if (c.walkRight) {
-      if (c === user && c.x + c.width === canvas.width / 2) {
-        levels[currentLevel].blocks.forEach((block) => {
+    if (c.walkRight && c.canWalkRight) {
+      if (c === user && c.x + c.width === canvas.width / 2 /*rightScrollMargin*/) {
+        levels[currentLevel].entities.blocks.forEach((block) => {
           block.x -= user.speed;
         });
-        levels[currentLevel].enemies.forEach((enemy) => {
+        levels[currentLevel].entities.enemies.forEach((enemy) => {
           enemy.x -= user.speed;
         });
       } else {
@@ -195,7 +207,7 @@ function move() {
 
 function getMobile() {
   mobile = [user];
-  levels[currentLevel].enemies.forEach((enemy) => {
+  levels[currentLevel].entities.enemies.forEach((enemy) => {
     if (enemy.x + enemy.width > 0 && enemy.x < 801 && (enemy.walkLeft || enemy.walkRight)) {
       mobile.push(enemy);
     }
@@ -207,7 +219,7 @@ function getOnScreenThings() {
   //   onScreenThings.push(thing)
   // });
   onScreenThings = mobile.concat();
-    levels[currentLevel].blocks.forEach((block) => {
+    levels[currentLevel].entities.blocks.forEach((block) => {
     if (block.x + block.width > 0 && block.x < 801) {
       onScreenThings.push(block);
     }
